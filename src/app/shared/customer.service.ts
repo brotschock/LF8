9190ceefa9bbfc89f9d0a9aaa0e6a06cdd2b624d
Jakob <jakob@localhost.localdomain>
2023-05-30 18:35:43 +0200

    successfully hosts frontend in vm, backend and client on physical host.

--- src/app/shared/customer.service.ts
+++ src/app/shared/customer.service.ts
@@ -9,7 +9,7 @@ import { ProjectList } from "./project";
   providedIn: "root"
 })
 export class CustomerService {
-  url: string = "http://localhost:8080";
+  url: string = "http://127.0.0.1:8080";
 
   constructor(private http: HttpClient) {}
 
