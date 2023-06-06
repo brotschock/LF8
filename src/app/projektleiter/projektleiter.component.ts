39742f8aeeb4e2688b0fa690d8ab7d4e2c2cbc7a
Jakob <jakobgerber@gmail.com>
2023-06-06 22:41:35 +0200

    implement password auth for projektleiter.

--- src/app/projektleiter/projektleiter.component.ts
+++ src/app/projektleiter/projektleiter.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { CustomerList } from "../shared/customer";
 import { CustomerService } from "../shared/customer.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-projektleiter",
@@ -10,11 +11,30 @@ import { CustomerService } from "../shared/customer.service";
 })
 export class ProjektleiterComponent implements OnInit {
   show = "buttons";
+  auth = false;
+  wrong = false;
+  authForm: FormGroup;
+
   customers$?: Observable<CustomerList>;
 
-  constructor(private cs: CustomerService) {}
+  constructor(private cs: CustomerService, private fb: FormBuilder) {
+    this.authForm = this.fb.group({password:["", Validators.required]});
+
+  }
 
   ngOnInit(): void {
     this.customers$ = this.cs.getAllCustomers();
+  }
+
+  checkPassword() {
+    const formValue = this.authForm.value;
+    const password = formValue.password;
+    this.authForm.reset();
+    if (password === "prolei") {
+      this.auth = true;
+      this.wrong = false;
+    } else {
+      this.wrong = true;
+    }
   }
 }
